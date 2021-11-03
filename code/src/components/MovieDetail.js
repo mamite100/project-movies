d518ded618eb4cbb5c54d9a216e78f53bbfe9f37
mamite100 <mamite.andersson@student.kyh.se>
2021-11-03 13:29:08 +0100

    mamies commit

--- code/src/components/MovieDetail.js
+++ code/src/components/MovieDetail.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import {Link, useParams } from "react-router-dom";
 
 const MovieDetail = () => {
   const { id } = useParams();
@@ -13,16 +13,31 @@ const MovieDetail = () => {
       .then((data) => setDetail(data));
   }, [id]);
 
-  console.log(detail);
+  if (detail === undefined){
+    return <></>
+  }
   return (
-    <div className="detail-container">
-      <img
+<section className="detail-container">
+<Link to='/' className='back-button'>
+<i className="fas fa-chevron-circle-left"></i> 
+<p>Movies</p>
+</Link>
+<div className="background" style= {{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0) 70%, rgb(0, 0, 0) 100%), url(https://image.tmdb.org/t/p/w1280${detail.backdrop_path})` }} alt={detail.title}>
+<div className='summary'>
+<img
         src={`https://image.tmdb.org/t/p/w780${detail.poster_path}`}
         alt="movie-poster"
       />
-      <div>{detail.overview}</div>
-      <div>{detail.vote_average}</div>
-    </div>
+      <div className='detail'>
+
+    <p> {detail.overview} <span className='rating'>{detail.vote_average}/10</span></p>
+    
+</div>
+</div>
+</div>
+     
+</section>
+
   );
 };
 export default MovieDetail;
